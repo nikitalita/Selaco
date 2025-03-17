e4c4f89a9e495a834a8bc68c81445412faae0a85
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-03-17 13:41:16 -0700

    Changes to compile 0.86G

--- build_osx.sh
+++ build_osx.sh
@@ -3,7 +3,7 @@ mkdir -p build
 cd build
 
 ZMUSICCONFIG=-DZMUSIC_INCLUDE_DIR=../bin/osx/zmusic/include\ -DZMUSIC_LIBRARIES=../bin/osx/zmusic/lib/libzmusic.dylib
-GZDOOMCONFIG="-DCMAKE_BUILD_TYPE=Release -DOSX_COCOA_BACKEND=OFF -DCMAKE_OSX_ARCHITECTURES=x86_64"
+GZDOOMCONFIG="-DCMAKE_BUILD_TYPE=Release -DOSX_COCOA_BACKEND=OFF -DCMAKE_OSX_ARCHITECTURES=arm64;x86_64"
 OSX_DYNAMIC_LIB=-DOSX_DYNAMIC_LIB=/opt/local/lib/libsndfile.1.dylib\;/opt/local/lib/libopenal.1.dylib\;/opt/local/lib/libMoltenVK.dylib
 cmake $GZDOOMCONFIG $ZMUSICCONFIG $OSX_DYNAMIC_LIB ..
 make -j 8
